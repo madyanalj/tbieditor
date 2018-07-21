de47efcdb3d0c61342cf5d855d55b33604beb13a
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-07-21 16:11:32 +0100

    config: added coverage threshold to jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['**/*.ts', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   globals: { 'ts-jest': { tsConfigFile: 'tsconfig.json' } },
   moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',

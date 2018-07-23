e2a1f084b9b5c01bff351f5d1c3591322b6aaac7
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-07-23 21:21:55 +0100

    bin: implemented tbieditor cli

---
+++ bin/tbieditor.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env ts-node
+
+import commander from 'commander'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { dirname, join } from 'path'
+import { transpile } from '../src/Transpiler'
+
+function ensureDirectoryExists(path: string): void {
+  const directory = dirname(path)
+  if (existsSync(directory)) return
+  ensureDirectoryExists(directory)
+  mkdirSync(directory)
+}
+
+commander
+  .version('1.0.0')
+  .usage('[file...]')
+  .option('-o, --outDir <directory>', 'destination directory for outputted files', '/')
+  .parse(process.argv)
+
+const inputFilenames = commander.args
+inputFilenames.forEach((inputFilename) => {
+  const input = readFileSync(inputFilename, 'utf8')
+  transpile(input, (outpulFilename, output) => {
+    const path = join(process.cwd(), commander.outDir, outpulFilename)
+    ensureDirectoryExists(path)
+    writeFileSync(path, output)
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.ts', '!**/*.d.ts'],
+  collectCoverageFrom: ['**/*.ts', '!**/*.d.ts', '!bin/*'],
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {

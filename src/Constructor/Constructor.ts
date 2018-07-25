1fcbaface032f1b275fe5e9e4f21d88d279c4ae6
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-07-25 22:56:18 +0100

    Constructor: added property to store location in input file

--- src/Constructor/Constructor.ts
+++ src/Constructor/Constructor.ts
@@ -1,6 +1,12 @@
 import { StateVariable, Store } from '../Store'
 
 abstract class Constructor {
+  public location?: {
+    start: number,
+    end: number,
+    line: number,
+    column: number,
+  }
   public abstract evaluate(store: Store): StateVariable
 }
 

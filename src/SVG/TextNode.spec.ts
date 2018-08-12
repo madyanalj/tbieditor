7ca5f605370b4cec5f8e2f4630cd357515f1f3cf
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-08-13 00:58:58 +0100

    SVG: removed alignement-baseline from text as it's not widely supported

--- src/SVG/TextNode.spec.ts
+++ src/SVG/TextNode.spec.ts
@@ -5,7 +5,7 @@ describe('TextNode', () => {
     it('should support text content', () => {
       const node = new TextNode()
       node.properties.content = 'Hello World!'
-      const expected = '<text font-family="sans-serif" alignment-baseline="hanging">Hello World!</text>'
+      const expected = '<text font-family="sans-serif">Hello World!</text>'
       expect(node.generate()).toBe(expected)
     })
   })

5c19b32b1cede1425e408a0c3dd0216b0deb71bc
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-08-12 17:47:44 +0100

    SVG: added alignment-baseline attribute to text node

--- src/SVG/TextNode.spec.ts
+++ src/SVG/TextNode.spec.ts
@@ -5,8 +5,8 @@ describe('TextNode', () => {
     it('should support text content', () => {
       const node = new TextNode()
       node.properties.content = 'Hello World!'
-      expect(node.generate())
-        .toBe('<text font-family="sans-serif">Hello World!</text>')
+      const expected = '<text font-family="sans-serif" alignment-baseline="hanging">Hello World!</text>'
+      expect(node.generate()).toBe(expected)
     })
   })
 })
--- src/SVG/TextNode.ts
+++ src/SVG/TextNode.ts
@@ -9,6 +9,7 @@ const defaultProperties = {
   'font-style': 'normal',
   'text-decoration': 'none',
   'text-anchor': 'start',
+  'alignment-baseline': 'auto',
   'letter-spacing': 'normal',
   'word-spacing': 'normal',
 }
@@ -17,6 +18,7 @@ class TextNode extends NonEmptyNode {
   public readonly properties: Properties = {
     ...defaultProperties,
     'font-family': 'sans-serif',
+    'alignment-baseline': 'hanging',
   }
   protected readonly defaultProperties = defaultProperties
   protected readonly TAG: string = 'text'

c541ce1c72ff6a15e14f9ca6216311321a1ae0a1
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-08-13 01:05:40 +0100

    e2e: added png output to kcl example

--- src/e2e.spec.ts
+++ src/e2e.spec.ts
@@ -1,14 +1,20 @@
 import { readFileSync } from 'fs'
+import { extname } from 'path'
 import { transpile } from './Transpiler'
 
 const examples = [
-  ['1-house.tbi', ['1-house.svg']],
-  ['2-kcl.tbi', ['2-kcl.svg']],
-  ['3-web-browser.tbi', [
-    '3-web-browser-1.svg',
-    '3-web-browser-2.svg',
-    '3-web-browser-3.svg',
-  ]],
+  [
+    '1-house.tbi',
+    ['1-house.svg'],
+  ],
+  [
+    '2-kcl.tbi',
+    ['2-kcl.svg', '2-kcl.png'],
+  ],
+  [
+    '3-web-browser.tbi',
+    ['3-web-browser-1.svg', '3-web-browser-2.svg', '3-web-browser-3.svg'],
+  ],
 ]
 
 const scripts: { [key: string]: string } = {}
@@ -16,6 +22,7 @@ examples.forEach(([name, outputFilenames]) => {
   scripts[name as string] = readFileSync(`examples/${name}`, 'utf8')
   const outNames = outputFilenames as string[]
   outNames.forEach((outName) => {
+    if (extname(outName) === '.png') return
     scripts[outName] = readFileSync(`examples/expected/${outName}`, 'utf8')
   })
 })
@@ -26,6 +33,8 @@ describe('e2e', () => {
       it('should produce correct output filename', () => {
         expect(expectedNames).toContain(filename)
       })
+
+      if (extname(filename) === '.png') return
 
       it(`should produce correct output content for ${filename}`, () => {
         expect(output).toBe(scripts[filename])

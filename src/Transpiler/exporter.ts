d788cbb4136f8e2e21d5804687d9ef3c04a381f6
Madyan Al-Jazaeri <22075444+madyanalj@users.noreply.github.com>
2018-08-20 20:53:13 +0100

    Transpiler: added doc comments

--- src/Transpiler/exporter.ts
+++ src/Transpiler/exporter.ts
@@ -1,19 +1,40 @@
 import { EventEmitter } from 'events'
 import { SVGNode } from '../SVG'
 
+/**
+ * Event emitter that manages image export event.
+ */
 class Exporter extends EventEmitter {
+  /**
+   * Exporter object singleton.
+   */
   public static exporter = new Exporter()
 
+  /**
+   * Registers a new listener to the export event.
+   * @param  listener Listerner function to be called when event is emitted.
+   * @return          Reference to the Exporter.
+   */
   public onExport(
     listener: (filename: string, svgNode: SVGNode) => void,
   ): this {
     return super.on('export', listener)
   }
 
+  /**
+   * Emits export event.
+   * @param  filename Filename of image to be exported.
+   * @param  svgNode  Root SVG node of the image to be exported.
+   * @return          True if the event had listeners, false otherwise.
+   */
   public emitExport(filename: string, svgNode: SVGNode): boolean {
     return super.emit('export', filename, svgNode)
   }
 
+  /**
+   * Removes all listeners for the export event.
+   * @return Reference to the Exporter.
+   */
   public stop(): this {
     return super.removeAllListeners('export')
   }
--- src/Transpiler/transpile.ts
+++ src/Transpiler/transpile.ts
@@ -2,6 +2,12 @@ import { parse } from '../Parser'
 import { Store } from '../Store'
 import { exporter } from './'
 
+/**
+ * Transpiles inputted script content into SVG and calls given callback
+ * function on every exported image.
+ * @param  input    Input script file content.
+ * @param  callback Callback function to be called for every exported image.
+ */
 function transpile(
   input: string,
   callback: (filename: string, output: string) => void,
